459d44b8fbf5108f35bcb3b39d7c6a42ee1eb72b
Lucas Gabriel <142439949+LucasGabriel408@users.noreply.github.com>
2025-09-30 11:06:41 -0300

    Add Task model, migration, seeder, and API routes
    Introduces a Task Eloquent model and migration for the tasks table. Seeds the database with sample tasks in DatabaseSeeder. Adds API test routes for summing numbers, returning user info, and paginating tasks in web.php.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Task;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,22 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Task::insert([
+            ['text' => 'Comprar leite'],
+            ['text' => 'Comprar pão'],
+            ['text' => 'Comprar café'],
+            ['text' => 'Comprar chocolate'],
+            ['text' => 'Comprar banana'],
+            ['text' => 'Comprar maçã'],
+            ['text' => 'Comprar uva'],
+            ['text' => 'Comprar abacaxi'],
+            ['text' => 'Comprar melancia'],
+            ['text' => 'Comprar melão'],
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Task;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Como é um início com testes de apenas testes de APIs vamos fazer o básico
+Route::get('/somar/{a}/{b}', function ($a, $b) {
+    return $a + $b;
+});
+
+Route::get('/info', function () {
+    $data = [
+        'name' => 'Fulano da Silva',
+        'age' => 20,
+        'email' => 'fulano@email.com',
+    ];
+
+    return response()->json($data, 200);
+});
+
+Route::get('/tasks', function () {
+    return  Task::where('id', '>', 2)->paginate(5);
 });
